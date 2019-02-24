5d0f12a4185e17485eab76774300b259203d87af
Andrew Sadowski <andrew.sadowski@developmentnow.com>
2019-02-24 13:00:37 -0800

    Added DragArea component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
+import {createStore} from 'redux';
+
 
 import ControlPanel from './components/ControlPanel'
+
 import logo from './logo.svg';
 import './App.css';
 
@@ -9,18 +12,6 @@ class App extends Component {
     return (
       <div className="App">
       <ControlPanel/>
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
       </div>
     );
   }
---
+++ src/components/DragArea.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+import { DragSource } from 'react-dnd';
+
+
+export default class DragArea extends Component {
+  render() {
+    return (
+      <div>
+        <p>DragArea</p>
+      </div>
+    )
+  }
+}
