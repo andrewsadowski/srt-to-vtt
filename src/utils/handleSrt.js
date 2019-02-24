198be4593ce3b590af7112f25f9812e7c19627d2
Andrew Sadowski <andrew.sadowski@developmentnow.com>
2019-02-23 18:35:27 -0800

    added comments regarding how to runb srt2vtt lib

--- src/utils/handleSrt.js
+++ src/utils/handleSrt.js
@@ -1,3 +1,13 @@
 const fs = require('fs');
 const srt2vtt = require('srt2vtt');
 
+
+
+/**
+ * how to run srt2vtt lib
+ * var srtData = fs.readFileSync('captions.srt');
+srt2vtt(srtData, function(err, vttData) {
+  if (err) throw new Error(err);
+  fs.writeFileSync('captions.vtt', vttData);
+});
+ */
