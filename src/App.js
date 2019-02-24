72f02e131014b3b61d102644a156a9ead584801a
Andrew Sadowski <andrew.sadowski@developmentnow.com>
2019-02-23 18:34:14 -0800

    removed tests directory, added package to handle srt2vtt conversion, added utils/handleSrt helper file

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+
+import ControlPanel from './components/ControlPanel'
 import logo from './logo.svg';
 import './App.css';
 
@@ -6,11 +8,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+      <ControlPanel/>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
+
           <a
             className="App-link"
             href="https://reactjs.org"
