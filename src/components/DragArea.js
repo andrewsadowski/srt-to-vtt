4f456b7f5af775c2778d72fc05113912748c4435
Andrew Sadowski <andrew.sadowski@developmentnow.com>
2019-03-10 17:02:00 -0700

    Updated DragArea to use dropzone, removed dnd imports

--- src/components/DragArea.js
+++ src/components/DragArea.js
@@ -1,12 +1,28 @@
 import React, { Component } from "react";
-import { DragSource, DragTarget } from "react-dnd";
+// import { DragSource, DragTarget } from "react-dnd";
+import Dropzone from "react-dnd-dropzone";
+import styled from "styled-components";
 
 export default class DragArea extends Component {
   render() {
     return (
-      <div>
-        <p>DragArea</p>
-      </div>
+      <DropContainer>
+        <Dropzone
+          onDrop={files => console.log(files)}
+          render={({ canDrop, isOver }) => (
+            <div>
+              Drop file here!
+              <pre>{JSON.stringify({ canDrop, isOver })}</pre>
+            </div>
+          )}
+        />
+      </DropContainer>
     );
   }
 }
+
+const DropContainer = styled.div`
+  width: 250px;
+  height: 250px;
+  background-color: palegoldenrod;
+`;
