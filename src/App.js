ab58768876eb1c3ed545faaf0c873082da65e91f
Andrew Sadowski <andrew.sadowski@developmentnow.com>
2019-03-10 22:24:08 -0700

    Added react-ionicons for dragarea animations

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 // import HTML5Backend from "react-dnd-html5-backend";
 // import { DragDropContext } from "react-dnd";
 import styled from "styled-components";
+import IosApertureOutline from "react-ionicons/lib/IosApertureOutline";
 
 import Header from "./components/Header";
 // import ControlPanel from "./components/ControlPanel";
@@ -12,6 +13,7 @@ class App extends Component {
     return (
       <AppContainer>
         <Header />
+        <IosApertureStyled rotate={true} beat={true} />
         <DragArea />
       </AppContainer>
     );
@@ -23,6 +25,11 @@ const AppContainer = styled.div`
   flex-direction: column;
   justify-content: center;
   align-items: center;
+`;
+
+const IosApertureStyled = styled(IosApertureOutline)`
+  height: 250px;
+  width: 250px;
 `;
 
 export default App;
