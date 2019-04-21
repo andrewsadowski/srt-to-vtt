8d676c2f9eb6dda44f5cf31440fd9da4717217b4
Andrew Sadowski <andrew.sadowski@developmentnow.com>
2019-04-21 14:27:33 -0700

    Added handleFiles method which maps props to state

--- src/App.js
+++ src/App.js
@@ -7,11 +7,21 @@ import Header from "./components/Header";
 import DragArea from "./components/DragArea";
 
 class App extends Component {
+  state = {
+    files: []
+  };
+
+  handleFiles = fileArray => {
+    this.setState({ files: fileArray }, () => {
+      console.log(this.state.files);
+    });
+  };
+
   render() {
     return (
       <AppContainer>
         <Header />
-        <DragArea />
+        <DragArea handleFiles={this.handleFiles} />
       </AppContainer>
     );
   }
