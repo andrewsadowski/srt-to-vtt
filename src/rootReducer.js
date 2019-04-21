6a517ea322ba00a46109061677a5f769278c5a46
Andrew Sadowski <andrew.sadowski@developmentnow.com>
2019-04-21 14:12:05 -0700

    Removed rootReducer from root

--- src/rootReducer.js
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "redux";
-//Import Reducers below
-import fileReducer from "./components/reducer";
-
-const rootReducer = combineReducers({ fileReducer });
-
-export default rootReducer;
