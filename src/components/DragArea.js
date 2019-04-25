ffc378661322898e0867a4a4b767cc15824decc6
Andrew Sadowski <andrew.sadowski@developmentnow.com>
2019-04-24 18:54:57 -0700

    Added triangle component to DragArea

--- src/components/DragArea.js
+++ src/components/DragArea.js
@@ -2,6 +2,8 @@ import React, { Component } from "react";
 import Dropzone from "react-dnd-dropzone";
 import styled from "styled-components";
 
+import DragTriangle from "./Triangle";
+
 export default class DragArea extends Component {
   render() {
     return (
@@ -12,6 +14,7 @@ export default class DragArea extends Component {
             <DropZoneContainer>
               Drop file here
               <pre>{JSON.stringify({ canDrop, isOver })}</pre>
+              <DragTriangle />
             </DropZoneContainer>
           )}
         />
