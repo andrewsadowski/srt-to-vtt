339b30f4e5803cf655f973eead6c4a3a396c8994
Andrew Sadowski <andrew.sadowski@developmentnow.com>
2019-04-24 18:55:18 -0700

    Updated App.js to include triangle

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import React, { Component } from "react";
 import styled from "styled-components";
-// import IosApertureOutline from "react-ionicons/lib/IosApertureOutline";
 
 import Header from "./components/Header";
-// import ControlPanel from "./components/ControlPanel";
 import DragArea from "./components/DragArea";
 
 class App extends Component {
@@ -33,10 +31,5 @@ const AppContainer = styled.div`
   justify-content: center;
   align-items: center;
 `;
-
-// const IosApertureStyled = styled(IosApertureOutline)`
-//   height: 250px;
-//   width: 250px;
-// `;
 
 export default App;
