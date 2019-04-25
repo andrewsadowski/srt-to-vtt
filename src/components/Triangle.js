b5a47db7798339b3e764b2e8c0bd41461171d038
Andrew Sadowski <andrew.sadowski@developmentnow.com>
2019-04-24 19:53:41 -0700

    Added styling and SC for SVG elements

--- src/components/Triangle.js
+++ src/components/Triangle.js
@@ -5,18 +5,28 @@ export default class DragTriangle extends Component {
   render() {
     return (
       <TriangleContainer>
-        <svg height="500" width="500">
+        <ActualTriangle height="500" width="500">
           <polygon points="250,60 100,400 400,400" class="triangle" />
-        </svg>
+        </ActualTriangle>
       </TriangleContainer>
     );
   }
 }
 
+const ActualTriangle = styled.svg`
+  fill: transparent;
+  stroke: black;
+  stroke-width: 8;
+  transition: all 0.8s ease-in-out;
+`;
+
 const TriangleContainer = styled.div`
   width: 100%;
-  height: 75%;
+  height: 100%;
   margin: auto;
   text-align: center;
   border: 1px solid white;
+  ${ActualTriangle}:hover {
+    transform: rotate(-180deg);
+  }
 `;
