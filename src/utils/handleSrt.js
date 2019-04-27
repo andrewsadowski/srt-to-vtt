5e7c839c3f6aad1233f7a6b03943f17474ff5417
Andrew Sadowski <andrew.sadowski@developmentnow.com>
2019-04-27 14:50:44 -0700

    removed commented out instructions at top of util file

--- src/utils/handleSrt.js
+++ src/utils/handleSrt.js
@@ -3,15 +3,6 @@ const path = window.require("path");
 const srt2vtt = window.require("srt2vtt");
 
 /**
- * how to run srt2vtt lib
- * var srtData = fs.readFileSync('captions.srt');
-srt2vtt(srtData, function(err, vttData) {
-  if (err) throw new Error(err);
-  fs.writeFileSync('captions.vtt', vttData);
-});
- */
-
-/**
  *
  * @param {string} filePath
  */
