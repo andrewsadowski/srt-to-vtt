afc6a59a0122bacba415f4a4c8a6c85c0eed2b9b
Andrew Sadowski <andrew.sadowski@developmentnow.com>
2019-05-05 20:30:53 -0700

    Added files props to OnDrop component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Header from "./components/Header";
 import DragArea from "./components/DragArea";
 import AtRest from "./components/AtRest";
 import Dropped from "./components/Dropped";
+import OnDrop from "./components/OnDrop";
 
 import { processSrtToVtt } from "./utils/handleSrt";
 
@@ -39,12 +40,15 @@ class App extends Component {
     let { hasDropOccurred } = this.state;
     return (
       <AppContainer>
-        <Header />
+        <Header style={{ position: "absolute" }} />
         <DragArea handleFiles={this.handleFiles}>
           {hasDropOccurred ? (
-            <Dropped dropped={this.state.hasDropOccurred} />
+            <OnDrop
+              dropped={this.state.hasDropOccurred}
+              files={this.state.files}
+            />
           ) : (
-            <AtRest />
+            <OnDrop dropped={this.state.hasDropOccurred} />
           )}
         </DragArea>
       </AppContainer>
