d79227d814445531de384ad07a050b72356df70f
Andrew Sadowski <andrew.sadowski@developmentnow.com>
2019-05-20 09:17:07 -0700

    Updated Styling of FileCard

--- src/components/FileCard.js
+++ src/components/FileCard.js
@@ -12,10 +12,26 @@ const FileCard = ({ name }) => {
 const FileCardContainer = styled.div`
   height: 15vh;
   width: 25vw;
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  border-radius: 10%;
+  background-color: #cb2d3e; /* fallback for old browsers */
+  background: -webkit-linear-gradient(
+    to right,
+    #ef473a,
+    #cb2d3e
+  ); /* Chrome 10-25, Safari 5.1-6 */
+  background: linear-gradient(
+    to right,
+    #ef473a,
+    #cb2d3e
+  ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
 `;
 
 const FileName = styled.p`
   font-family: "Exo", sans-serif;
+  text-align: center;
 `;
 
 export default FileCard;
