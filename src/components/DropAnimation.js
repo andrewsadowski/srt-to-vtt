70e68faf3e52109725343945588f263df00b0f9d
Andrew Sadowski <andrew.sadowski@developmentnow.com>
2019-05-20 21:10:29 -0700

    Updated dropAnim to drop down an arrow transition animation;

--- src/components/DropAnimation.js
+++ src/components/DropAnimation.js
@@ -1,20 +1,35 @@
 import React from "react";
-import { useSpring, animation } from "react-spring";
+import { useTransition, animated } from "react-spring";
 import styled from "styled-components";
 
 const DropAnimation = ({ hovered }) => {
-  const animation = useSpring({});
+  const animation = useTransition(hovered, null, {
+    from: { opacity: 0.7, marginTop: "-40px" },
+    enter: { opacity: 1, marginTop: "20px" },
+    leave: { opacity: 1, marginTop: "-40px" },
+    config: { mass: 15, tension: 370, friction: 30 }
+  });
+  console.log(hovered);
   return (
-    <div>
-      <Triangle />
-    </div>
+    <Container>
+      {animation.map(
+        ({ item, key, props }) => item && <Triangle key={key} style={props} />
+      )}
+    </Container>
   );
 };
 
-const Triangle = styled.div`
+const Container = styled(animated.div)`
+  display: flex;
+  justify-content: center;
+  align-content: center;
+  align-items: center;
+`;
+
+const Triangle = styled(animated.div)`
   border-color: black transparent;
   border-style: solid;
-  border-width: 20px 20px 0px 20px;
+  border-width: 100px 100px 0px 100px;
   justify-self: center;
   align-self: center;
   height: 0px;
