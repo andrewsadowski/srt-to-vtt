2a16fe416e02a2b29ac10b63c066cfe3d042cb6c
Andrew Sadowski <andrew.sadowski@developmentnow.com>
2019-05-23 20:49:46 -0700

    Updated styling for fileCard

--- src/components/FileCard.js
+++ src/components/FileCard.js
@@ -18,21 +18,11 @@ const FileCardContainer = styled.div`
   align-content: center;
   border-radius: 30px;
   margin-bottom: 10px;
-  background-color: #cb2d3e; /* fallback for old browsers */
-  background: -webkit-linear-gradient(
-    to right,
-    #ef473a,
-    #cb2d3e
-  ); /* Chrome 10-25, Safari 5.1-6 */
-  background: linear-gradient(
-    to right,
-    #ef473a,
-    #cb2d3e
-  ); /* W3C, IE 10+/ Edge, Firefox 16+, Chrome 26+, Opera 12+, Safari 7+ */
 `;
 
 const FileName = styled.p`
   font-family: "Exo", sans-serif;
+  justify-self: center;
   text-align: center;
 `;
 // const FilePath = styled.p`
