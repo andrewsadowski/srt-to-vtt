ce64f60158036078f21fe2a3d1f93d4433daa761
Andrew Sadowski <andrew.sadowski@developmentnow.com>
2019-05-26 14:34:37 -0700

    Updated styling of filecard and added state hook and useSpring hook

--- src/components/FileCard.js
+++ src/components/FileCard.js
@@ -4,8 +4,12 @@ import { useSpring, animated } from "react-spring";
 
 const FileCard = ({ name, path }) => {
   const [on, setOn] = useState(false);
+  const animation = useSpring({
+    backgroundColor: on ? "#ffdc00" : "#8e9eab"
+  });
   return (
     <FileCardContainer
+      style={animation}
       onMouseLeave={() => setOn(false)}
       onMouseOver={() => setOn(true)}
     >
@@ -14,14 +18,15 @@ const FileCard = ({ name, path }) => {
   );
 };
 
-const FileCardContainer = styled.div`
-  height: 15vh;
-  width: 75%;
+const FileCardContainer = styled(animated.div)`
+  height: auto;
+  width: 90%;
   display: flex;
   justify-content: center;
   align-content: center;
   border-radius: 30px;
   margin-bottom: 10px;
+  border: 1px solid #ffdc00;
 `;
 
 const FileName = styled.p`
